c6a04822abefb09b164b989ddaffe5e5c3b59a41
Ninon <ninon.slg@mail.com>
2023-05-05 11:09:58 +0200

    formulaire de connexion

--- src/domo/general/endpoints.py
+++ src/domo/general/endpoints.py
@@ -4,6 +4,7 @@ from flask import (
     session,
     url_for,
     render_template,
+    request
 )
 
 gen_bp = Blueprint(
@@ -17,8 +18,21 @@ gen_bp = Blueprint(
 @gen_bp.route("/")
 def home():
     """renvoie vers l'écran d'accueil"""
-    return render_template("acceuil.html")
+    return render_template("auth_connexion.html")
 
 @gen_bp.app_errorhandler(404)
 def page_non_trouve(error):
     return render_template("404.html"), 404
+
+dicomdp = {"login@g.com" : "mdp"}
+@gen_bp.route("/acceuil", methods=['POST'])
+def validermdp():
+    entree = request.form
+    entreemail = entree['mail']
+    entreemdp = entree['mdp']
+    for mail in dicomdp:
+        if mail == entreemail and dicomdp[mail] == entreemdp:
+            return render_template("acceuil.html")
+    else : 
+        return render_template("auth_connexion.html")
+        
